5ee86c704a47c2c24a2ba39e064511aaa09224e2
hmglasgow <harry.glasgow@pm.me>
2022-12-11 11:26:09 +0800

    time

--- app/src/main/java/com/example/tasklist/AddEditActivity.kt
+++ app/src/main/java/com/example/tasklist/AddEditActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
